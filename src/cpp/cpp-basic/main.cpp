80b6e86fbeda4c45290c576b054ba42ec8aedb6c
Rezal <rladlfdnd369@gmail.com>
2024-06-17 17:31:24 +0900

    d

---
+++ src/cpp/cpp-basic/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "CPP Basic Learn" << endl;
+
+    return 0;
+}
+
+/**
+ * 1. 확장(Extension) 설치
+ * 2. C/C++ Edit Configuration 설정
+ * 3. Tasks 설정
+ * 4. Launch 설정
+ */
